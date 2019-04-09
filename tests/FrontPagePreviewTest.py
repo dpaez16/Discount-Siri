4e3a8c615236332a142ebc6df67fc4e61ecae96c
Danny Paez <dpaez16@yahoo.com>
2019-04-09 12:57:29 -0500

    finish front page later

---
+++ tests/FrontPagePreviewTest.py
@@ -0,0 +1,28 @@
+import unittest
+from modules.front_page_preview import get_front_page_preview
+
+
+class RandomFactTests(unittest.TestCase):
+    """
+    Runs all tests to make sure that the front page preview function works as expected.
+    """
+
+    def test_proper_format(self):
+        """
+        Tests to see if the front page preview function works as expected.
+        """
+
+        front_page_preview = get_front_page_preview()
+        self.assertEqual(len(front_page_preview), 20)
+
+        for post in front_page_preview:
+            self.assertTrue('subreddit' in post)
+            self.assertTrue('title' in post)
+            self.assertTrue('preview_url' in post)
+            self.assertTrue('post_link' in post)
+            self.assertTrue('upvotes' in post)
+        return
+
+
+if __name__ == '__main__':
+    unittest.main()
